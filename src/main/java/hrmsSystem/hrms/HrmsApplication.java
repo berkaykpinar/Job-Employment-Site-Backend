0ea517c601c0d894c78ef57fbade37a0c7d33f57
Berkay Akpınar <berkayakpinar1998@gmail.com>
2021-06-17 19:21:31 +0300

    swagger added

--- src/main/java/hrmsSystem/hrms/HrmsApplication.java
+++ src/main/java/hrmsSystem/hrms/HrmsApplication.java
@@ -2,12 +2,24 @@ package hrmsSystem.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class HrmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
+	}
+
+	@Bean //bütün apileri tarar dökümantasyona ekle
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("hrmsSystem.hrms"))
+				.build();
 	}
 
 }
